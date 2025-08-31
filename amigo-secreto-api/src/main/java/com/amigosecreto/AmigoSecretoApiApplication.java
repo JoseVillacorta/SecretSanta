868e4e7cf19f303373655cb8ca9fb22b803bbb06
Jose Villacorta <villacortafur@gmail.com>
2025-08-31 01:57:45 -0500

    Initial Spring Boot backend project

---
+++ amigo-secreto-api/src/main/java/com/amigosecreto/AmigoSecretoApiApplication.java
@@ -0,0 +1,13 @@
+package com.amigosecreto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoSecretoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigoSecretoApiApplication.class, args);
+	}
+
+}
---
+++ amigo-secreto-api/src/test/java/com/amigosecreto/AmigoSecretoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amigosecreto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoSecretoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
